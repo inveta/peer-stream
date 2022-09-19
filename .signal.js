a5029594e246d4f7ee7450db8ac2c819e307e6de
XOSG <2195868682@qq.com>
2022-09-19 15:24:43 +0800

    Update .signal.js

--- .signal.js
+++ .signal.js
@@ -5,7 +5,7 @@ process.env.player = 88;
 // UE5端口
 process.env.engine = 8888;
 
-process.env.token = "/hello";
+// process.env.token = "/hello";
 process.env.limit = 4;
 process.env.throttle = true;
 
@@ -13,12 +13,14 @@ process.env.throttle = true;
 process.env.http = true;
 
 // 一对一
-// process.env.one2one = true;
+process.env.one2one = true;
 
 // 自启动脚本池，以"UE5_"开头
 process.env.UE5_GPU_0 = "start C:/Users/admin/Desktop/Windows/demo.exe -Unattended -RenderOffScreen -PixelStreamingURL=ws://127.0.0.1:8888/ -GraphicsAdapter=0 -ProjectID=GPU_0";
 process.env.UE5_GPU_1 = "start C:/Users/admin/Desktop/Windows/demo.exe -Unattended -RenderOffScreen -PixelStreamingURL=ws://127.0.0.1:8888/ -GraphicsAdapter=0 -ProjectID=GPU_1";
+process.env.UE5_GPU_2 = "start C:/Users/admin/Desktop/Windows/demo.exe -Unattended -RenderOffScreen -PixelStreamingURL=ws://127.0.0.1:8888/ -GraphicsAdapter=0 -ProjectID=GPU_2";
 
+// process.env.UE5_GZA = 'curl http://v4.gza.com/run_UE5'
 
 require("./signal.js");
 
