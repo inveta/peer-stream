0a2661ac988c5bcdcf6fa1c10111df113e64facf
XOSG <2195868682@qq.com>
2022-09-19 13:54:48 +0800

    asdf

--- README.md
+++ README.md
@@ -1,40 +1,47 @@
-# peer-stream.js
+# UE5 Pixel Streaming
 
 Compared to EpicGame's heavily-designed SDK for Pixel Streaming, peer-stream.js is a lightweight WebRTC library with 0 dependency, containing a frontend component (using WebComponents API), along with a signaling server (using NodeJS).
 
-- peer-stream.js: frontend SDK.
-- signal.js: signaling server.
-- signal-pro.js: signaling server with advanced features.
-- package.json: maintain all parameters.
-- test.html: frontend page.
+- peer-stream.js: browser SDK for player.
+- signal.js: node.js signaling server.
+- .signal.js: signal.js with env variables.
+- test.html: browser web page.
 
-## Signaling Server
+## Demo
 
-```
+```s
 # install WebSocket
 npm install ws@8.5.0
 
 # start Signaling Server
-node signal.js player=88 engine=8888
+player=88 engine=8888 node signal.js
 
-# or start Signaling Server Pro
-node signal-pro.js player=88 engine=8888
+# visit webpage
+start http://localhost:88/test.html
 ```
 
-## signal-pro.js
+## Env Variables for signal.js
 
-- require("signal.js");
-- authenticate player with token before connection;
-- limit max number of players connected;
-- WebSocket throttle, prevent frequent reconnection; (or UE5 will crash)
-- start UE5 automatically when player connected; (using package.json)
-- serve HTTP static files; (share same port with WebSocket)
+| env      | type      | default   | usage                                                     |
+| -------- | --------- | --------- | --------------------------------------------------------- |
+| player   | number    | 88        | WS port number for player                                 |
+| engine   | number    | 8888      | WS port number for UE5                                    |
+| http     | bool      | false     | serve http static files (same port with player)           |
+| UE5_*    | string[ ] |           | run command when player connected (e.g. starting UE5.exe) |
+| one2one  | bool      | false     | one-to-one mapping for player & UE5                       |
+| token    | string    |           | authenticate the player                                   |
+| limit    | number    | +Infinity | limit max number of players connected                     |
+| throttle | bool      | false     | WebSocket throttle, prevent frequent reconnection         |
+
+## Load Balance in signal.js
+
+signal.js accept multi UE5 & player connections, where each UE5 maps to multi-players with load-balancing. Turn `one2one` on to keep one-to-one mapping. Provide `UE5_*` to start UE5 automatically. More detailed example in `.signal.js`.
 
 ## Unreal Engine
 
 enable the plugin:
 
-```
+```s
 Plugins > Built-In > Graphics > Pixel Streaming > Enabled
 Editor Preferences > Level Editor > Play > Additional Launch Parameters
 start myPackagedGame.exe -{key}={value}
@@ -42,11 +49,11 @@ start myPackagedGame.exe -{key}={value}
 
 common startup options:
 
-```
+```s
  -PixelStreamingURL="ws://localhost:8888"
  -RenderOffScreen
  -Unattended
- -graphicsadapter=0
+ -GraphicsAdapter=0
  -ForceRes
  -windowed
  -ResX=1280
@@ -56,18 +63,18 @@ common startup options:
  -PixelStreamingEncoderRateControl=VBR
 ```
 
-## Browser
+## peer-stream.js
 
 HTML:
 
-```
+```html
 <script src="peer-stream.js"></script>
 <video is="peer-stream" signal="ws://127.0.0.1:88/hello"></video>
 ```
 
 or JavaScript:
 
-```
+```html
 <script type="module">
 import "peer-stream.js";
 const ps = document.createElement("video", { is: "peer-stream" });
@@ -76,33 +83,21 @@ document.body.append(ps);
 </script>
 ```
 
-## Messages
+### Messages
 
 sending messages:
 
-```
+```js
 // object will be JSON.stringify()
 ps.emitMessage(msg: string | object);
 ```
 
 receiving messages:
 
-```
+```js
 ps.addEventListener("message", e => {
     e.detail;   // string
 });
-```
-
-## Common Commands
-
-```
-ps.debug('PLAYER.clients.size')   // show players count
-ps.debug('PLAYER.clients.forEach(p=>p.playerId!==playerId&&p.close(1011,"Infinity"));limit=1;')  // kick other players
-ps.debug('[...PLAYER.clients].map(x=>x.req.socket.remoteAddress)')  // every player's IP
-ps.debug('playerId')  // show my id
-console.log(ps.pc.remoteDescription.sdp)    // show peer info
-ps.addEventListener('mouseenter',()=>{ps.focus();ps.requestPointerLock()})    // pointer lock
-ps.style.pointerEvents='none'   // read only <video>
 ```
 
 ## Requirement
--- signal.js
+++ signal.js
@@ -3,88 +3,88 @@
 const { Server } = require("ws");
 
 global.ENGINE = new Server(
-  { port: +process.env.engine || 8888, clientTracking: true },
-  () => { }
+	{ port: +process.env.engine || 8888, clientTracking: true },
+	() => { }
 );
 
 ENGINE.on("connection", (ue, req) => {
-  ue.req = req;
+	ue.req = req;
 
-  ue.fe = new Set()
-  // sent to UE5 as initial signal
-  ue.send(
-    JSON.stringify({
-      type: "config",
-      peerConnectionOptions: {
-        // iceServers: [{
-        //     urls: [
-        //       "stun:stun.l.google.com:19302",
-        //       "stun:stun1.l.google.com:19302",
-        //       "stun:stun2.l.google.com:19302",
-        //       "stun:stun3.l.google.com:19302",
-        //       "stun:stun4.l.google.com:19302",
-        // ],},],
-      },
-    })
-  );
+	ue.fe = new Set()
+	// sent to UE5 as initial signal
+	ue.send(
+		JSON.stringify({
+			type: "config",
+			peerConnectionOptions: {
+				// iceServers: [{
+				//     urls: [
+				//       "stun:stun.l.google.com:19302",
+				//       "stun:stun1.l.google.com:19302",
+				//       "stun:stun2.l.google.com:19302",
+				//       "stun:stun3.l.google.com:19302",
+				//       "stun:stun4.l.google.com:19302",
+				// ],},],
+			},
+		})
+	);
 
-  // 认领空闲的前端们
-  for (const fe of PLAYER.clients) {
-    if (!fe.ue) {
-      fe.ue = ue;
-      ue.fe.add(fe)
-      ue.send(
-        JSON.stringify({
-          type: "playerConnected",
-          playerId: fe.req.socket.remotePort,
-          dataChannel: true,
-          sfu: false,
-        })
-      );
-    }
-  }
-  print();
+	// 认领空闲的前端们
+	for (const fe of PLAYER.clients) {
+		if (!fe.ue) {
+			fe.ue = ue;
+			ue.fe.add(fe)
+			ue.send(
+				JSON.stringify({
+					type: "playerConnected",
+					playerId: fe.req.socket.remotePort,
+					dataChannel: true,
+					sfu: false,
+				})
+			);
+		}
+	}
+	print();
 
-  ue.on("message", (msg) => {
-    msg = JSON.parse(msg);
+	ue.on("message", (msg) => {
+		msg = JSON.parse(msg);
 
 
-    // Convert incoming playerId to a string if it is an integer, if needed. (We support receiving it as an int or string).
+		// Convert incoming playerId to a string if it is an integer, if needed. (We support receiving it as an int or string).
 
-    if (msg.type === "ping") {
-      ue.send(JSON.stringify({ type: "pong", time: msg.time }));
-      return;
-    }
+		if (msg.type === "ping") {
+			ue.send(JSON.stringify({ type: "pong", time: msg.time }));
+			return;
+		}
 
-    // player's port as playerID
-    const fe = [...ue.fe].find((fe) =>
-      fe.req.socket.remotePort === +msg.playerId
-    );
-    if (!fe) {
-      // console.error("? player not found:", playerId);
-      return;
-    }
+		// player's port as playerID
+		const fe = [...ue.fe].find((fe) =>
+			fe.req.socket.remotePort === +msg.playerId
+		);
+		if (!fe) {
+			// console.error("? player not found:", playerId);
+			return;
+		}
 
-    delete msg.playerId; // no need to send it to the player
-    if (["offer", "answer", "iceCandidate"].includes(msg.type)) {
-      fe.send(JSON.stringify(msg));
-    } else if (msg.type === "disconnectPlayer") {
-      fe.close(1011, msg.reason);
-    } else {
-      // console.error("? invalid Engine message type:", msg.type);
-    }
-  });
+		delete msg.playerId; // no need to send it to the player
+		if (["offer", "answer", "iceCandidate"].includes(msg.type)) {
+			fe.send(JSON.stringify(msg));
+		} else if (msg.type === "disconnectPlayer") {
+			fe.close(1011, msg.reason);
+		} else {
+			// console.error("? invalid Engine message type:", msg.type);
+		}
+	});
 
-  ue.on("close", (code, reason) => {
+	ue.on("close", (code, reason) => {
 
-    ue.fe.forEach(fe => fe.ue = null)
-    print();
-  });
+		ue.fe.forEach(fe => fe.ue = null)
+		print();
+	});
 
-  ue.on("error", (error) => {
-    // A control frame must have a payload length of 125 bytes or less
-    // ue.close(1011, error.message.slice(0, 100));
-  });
+	ue.on("error", (error) => {
+		// A control frame must have a payload length of 125 bytes or less
+		// ue.close(1011, error.message.slice(0, 100));
+	});
 
 
 });
@@ -121,139 +121,137 @@ ENGINE.on("connection", (ue, req) => {
 
 
 function onRequest(req, res) {
-  // websocket请求时不触发
-  // serve HTTP static files
+	// websocket请求时不触发
+	// serve HTTP static files
 
-  const read = require("fs").createReadStream(
-    require("path").join(__dirname, req.url)
-  );
+	const read = require("fs").createReadStream(
+		require("path").join(__dirname, req.url)
+	);
 
-  read.on("error", (err) => {
-    res.end(err.message);
-  }).on("ready", () => {
-    read.pipe(res);
-  });
+	read.on("error", (err) => {
+		res.end(err.message);
+	}).on("ready", () => {
+		read.pipe(res);
+	});
 }
 
 
 
-const pool = Object.entries(process.env)
-  .filter(([key]) => key.startsWith('UE5_'))
-  .map(([, value]) => value)
+const UE5_pool = Object.entries(process.env)
+	.filter(([key]) => key.startsWith('UE5_'))
+	.map(([, value]) => value)
 
 // front end
 global.PLAYER = new Server({
-  server: require("http")
-    .createServer(process.env.http ? onRequest : undefined)
-    .listen(+process.env.player || 88, () => { }),
-  // port:   88,
-  clientTracking: true,
+	server: require("http")
+		.createServer(process.env.http ? onRequest : undefined)
+		.listen(+process.env.player || 88, () => { }),
+	// port:   88,
+	clientTracking: true,
 });
 // every player
 PLAYER.on("connection", (fe, req) => {
-  fe.req = req;
+	fe.req = req;
 
-  if (process.env.one2one) {
-    fe.ue = [...ENGINE.clients].find(ue => ue.fe.size === 0)
-  } else {
-    fe.ue = [...ENGINE.clients].sort((a, b) => a.fe.size - b.fe.size)[0]
-  }
-
-
-  // password
-  if (process.env.token) {
-    if (req.url !== process.env.token) {
-      fe.close();
-      return;
-    }
-  }
-
-  // players max count
-  if (process.env.limit) {
-    if (PLAYER.clients.size > +process.env.limit) {
-      fe.close();
-      return;
-    }
-  }
-
-  // throttle
-  if (process.env.throttle) {
-    if (global.throttle) {
-      fe.close();
-      return;
-    } else {
-      global.throttle = true;
-      setTimeout(() => {
-        global.throttle = false;
-      }, 500);
-    }
-  }
+	if (process.env.one2one) {
+		fe.ue = [...ENGINE.clients].find(ue => ue.fe.size === 0)
+	} else {
+		fe.ue = [...ENGINE.clients].sort((a, b) => a.fe.size - b.fe.size)[0]
+	}
 
 
+	// password
+	if (process.env.token) {
+		if (req.url !== process.env.token) {
+			fe.close();
+			return;
+		}
+	}
+
+	// players max count
+	if (process.env.limit) {
+		if (PLAYER.clients.size > +process.env.limit) {
+			fe.close();
+			return;
+		}
+	}
+
+	// throttle
+	if (process.env.throttle) {
+		if (global.throttle) {
+			fe.close();
+			return;
+		} else {
+			global.throttle = true;
+			setTimeout(() => {
+				global.throttle = false;
+			}, 500);
+		}
+	}
 
 
 
 
-  if (fe.ue) {
-    fe.ue.fe.add(fe)
-    fe.ue.send(
-      JSON.stringify({
-        type: "playerConnected",
-        playerId: req.socket.remotePort,
-        dataChannel: true,
-        sfu: false,
-      })
-    );
-  } else if (pool.length) {
-    require("child_process").exec(
-      pool[0],
-      { cwd: __dirname, },
-      (err, stdout, stderr) => { }
-    );
-    pool.push(pool.shift())
-
-  }
 
 
+	if (fe.ue) {
+		fe.ue.fe.add(fe)
+		fe.ue.send(
+			JSON.stringify({
+				type: "playerConnected",
+				playerId: req.socket.remotePort,
+				dataChannel: true,
+				sfu: false,
+			})
+		);
+	} else if (UE5_pool.length) {
+		require("child_process").exec(
+			UE5_pool[0],
+			{ cwd: __dirname, },
+			(err, stdout, stderr) => { }
+		);
+		UE5_pool.push(UE5_pool.shift())
 
-  print();
+	}
 
 
 
-  fe.on("message", (msg) => {
-    if (!fe.ue) {
-      fe.send(`! Engine not ready`);
-      return;
-    }
-
-    msg = JSON.parse(msg);
-
-    // console.log("player", playerId, msg.type);
-
-    msg.playerId = req.socket.remotePort;
-    if (["answer", "iceCandidate"].includes(msg.type)) {
-      fe.ue.send(JSON.stringify(msg));
-    } else {
-      fe.send("? " + msg.type);
-    }
-  });
-
-  fe.on("close", (code, reason) => {
-    if (fe.ue) {
-      fe.ue.send(JSON.stringify({
-        type: "playerDisconnected",
-        playerId: req.socket.remotePort
-      }));
-      fe.ue.fe.delete(fe)
-    }
+	print();
 
 
-    print();
-  });
 
-  fe.on("error", (error) => {
-    // console.error("! player", playerId, "connection error:", error);
-  });
+	fe.on("message", (msg) => {
+		if (!fe.ue) {
+			fe.send(`! Engine not ready`);
+			return;
+		}
+
+		msg = JSON.parse(msg);
+
+		msg.playerId = req.socket.remotePort;
+		if (["answer", "iceCandidate"].includes(msg.type)) {
+			fe.ue.send(JSON.stringify(msg));
+		} else {
+			fe.send("? " + msg.type);
+		}
+	});
+
+	fe.on("close", (code, reason) => {
+		if (fe.ue) {
+			fe.ue.send(JSON.stringify({
+				type: "playerDisconnected",
+				playerId: req.socket.remotePort
+			}));
+			fe.ue.fe.delete(fe)
+		}
+
+
+		print();
+	});
+
+	fe.on("error", (error) => {
+		// console.error("! player", playerId, "connection error:", error);
+	});
 
 
 
@@ -261,44 +259,44 @@ PLAYER.on("connection", (fe, req) => {
 
 // keep alive
 setInterval(() => {
-  for (const fe of PLAYER.clients) {
-    fe.send("ping");
-  }
+	for (const fe of PLAYER.clients) {
+		fe.send("ping");
+	}
 }, 50 * 1000);
 
 // 打印映射关系
 function print() {
-  console.clear();
-  console.log()
+	console.clear();
+	console.log()
 
-  ENGINE.clients.forEach(ue => {
-    console.log(
-      ue.req.socket.remoteAddress,
-      ue.req.socket.remotePort,
-      ue.req.url
-    );
-    ue.fe.forEach(fe => {
-      console.log(
-        '     ',
-        fe.req.socket.remoteAddress,
-        fe.req.socket.remotePort,
-        fe.req.url
-      );
-    })
-  })
+	ENGINE.clients.forEach(ue => {
+		console.log(
+			ue.req.socket.remoteAddress,
+			ue.req.socket.remotePort,
+			ue.req.url
+		);
+		ue.fe.forEach(fe => {
+			console.log(
+				'     ',
+				fe.req.socket.remoteAddress,
+				fe.req.socket.remotePort,
+				fe.req.url
+			);
+		})
+	})
 
-  const fe = [...PLAYER.clients].filter(fe => !fe.ue)
-  if (fe.length) {
-    console.log("idle players:");
-    fe.forEach(fe => {
-      console.log(
-        '     ',
-        fe.req.socket.remoteAddress,
-        fe.req.socket.remotePort,
-        fe.req.url
-      );
-    })
-  }
+	const fe = [...PLAYER.clients].filter(fe => !fe.ue)
+	if (fe.length) {
+		console.log("idle players:");
+		fe.forEach(fe => {
+			console.log(
+				'     ',
+				fe.req.socket.remoteAddress,
+				fe.req.socket.remotePort,
+				fe.req.url
+			);
+		})
+	}
 
 
 }
