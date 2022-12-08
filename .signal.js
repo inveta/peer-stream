a7d1b34702666834223ff8ff7804a953ad4186c6
XOSG <2195868682@qq.com>
2022-12-08 18:08:11 +0800

    1

--- .signal.js
+++ .signal.js
@@ -1,4 +1,4 @@
-"http://localhost:88/test.html";
+require('child_process').exec(`start http://localhost:88/test.html`);
 
 // 设置环境变量
 
@@ -14,8 +14,8 @@ process.env.throttle = true;
 
 // 自启动脚本池，以"UE5_"开头
 process.env.UE5_GPU_0 = "start C:/Users/admin/Desktop/Windows/demo.exe -Unattended -RenderOffScreen -PixelStreamingURL=ws://127.0.0.1:88/ -GraphicsAdapter=0 -ProjectID=GPU_0";
-process.env.UE5_GPU_1 = "start C:/Users/admin/Desktop/Windows/demo.exe -Unattended -RenderOffScreen -PixelStreamingURL=ws://127.0.0.1:88/ -GraphicsAdapter=0 -ProjectID=GPU_1";
-process.env.UE5_GPU_2 = "start C:/Users/admin/Desktop/Windows/demo.exe -Unattended -RenderOffScreen -PixelStreamingURL=ws://127.0.0.1:88/ -GraphicsAdapter=0 -ProjectID=GPU_2";
+process.env.UE5_GPU_1 = "start C:/Users/admin/Desktop/Windows/demo.exe -Unattended -RenderOffScreen -PixelStreamingURL=ws://127.0.0.1:88/ -GraphicsAdapter=1 -ProjectID=GPU_1";
+process.env.UE5_GPU_2 = "start C:/Users/admin/Desktop/Windows/demo.exe -Unattended -RenderOffScreen -PixelStreamingURL=ws://127.0.0.1:88/ -GraphicsAdapter=2 -ProjectID=GPU_2";
 
 // process.env.UE5_10_0_42_16 = 'curl http://10.0.42.16/RUN-UE5'
 
